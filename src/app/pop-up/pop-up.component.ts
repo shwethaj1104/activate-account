f59574f9d2ef2e3479b42df1c192f4bcb294bdd8
shwethaj1104 <shwethaj1104@gmail.com>
2022-10-08 15:29:17 +0530

    assignment completed

--- src/app/pop-up/pop-up.component.ts
+++ src/app/pop-up/pop-up.component.ts
@@ -27,20 +27,16 @@ export class PopUpComponent implements OnInit {
   inputChangeemail(event: any) {
     let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (pattern.test(this.form.value.email)) {
-
       this.emailid = true
     } else {
-
       this.emailid = false
     }
-
   }
   inputChangePhone(event: any) {
     if (this.form.value.phone.length === 10) {
       this.phonenumber = true
     } else {
-
-      this.emailid = false
+      this.phonenumber = false
     }
 
   }
