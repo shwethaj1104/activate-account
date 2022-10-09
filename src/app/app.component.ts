9bc664a1ab5a89d08ee6fd8f7390783b9be3fcca
shwethaj1104 <shwethaj1104@gmail.com>
2022-10-09 11:04:53 +0530

    Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PopUpComponent } from './pop-up/pop-up.component';
 import { MatDialog } from '@angular/material/dialog';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -11,18 +10,15 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'activate';
-  constructor(private modal:MatDialog,
+  constructor(private modal: MatDialog,
     private modalService: NgbModal
-    ) { }
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
 
-  }
-  
-  activate(){
+  activate() {
     this.modalService.open(PopUpComponent);
-
   }
 }
